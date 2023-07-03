955ddfee8399bf6be3a97c3d53a9a9f8d4a21896
devblulab <116671864+devblulab@users.noreply.github.com>
2023-07-03 14:30:38 +0100

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,20 @@ export default class MyDocument extends Document {
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2544082529181732"
             crossOrigin="anonymous"
           ></script>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-30JKXJ3WJS"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-30JKXJ3WJS');
+              `,
+            }}
+          ></script>
         </Head>
         <body>
           <Main />
